7eee3004b6d1169718419f98b7f7dae74565f9b8
mrdil <mrdil@BMW>
2023-12-09 22:19:04 +0500

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace module15_dz
+{
+    class Program
+    {
+        static void Main()
+        {
+            Type consoleType = typeof(Console);
+            MethodInfo[] methods = consoleType.GetMethods();
+
+            Console.WriteLine("Методы класса Console:");
+            foreach (var method in methods)
+            {
+                Console.WriteLine(method.Name);
+            }
+        }
+    }
+}
